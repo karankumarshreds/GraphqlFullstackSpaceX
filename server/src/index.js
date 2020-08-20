ef43859d9a0f147a2793b3c16e3132355a48e5cd
Karan Kumar <nailedITnerd@gmail.com>
2020-08-21 00:17:49 +0530

    add sql datasource

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,34 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
+const { createStore } = require('./utils');
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+// function to setup our SQLite database 
+const store = createStore();
 
 // Server setup
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({
+    typeDefs,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+    })
+});
+
 server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
 });
 
 
+
+/**************************** NOTE ********************************
+ * If you use this.context in a datasource, it's critical to create
+ * a new instance in the dataSources function, rather than sharing a
+ * single instance. Otherwise, initialize might be called during the
+ * execution of asynchronous code for a particular user, replacing
+ * this.context with the context of another user.
+ */
 
 
